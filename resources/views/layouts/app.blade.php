ad8f1664898476e1d34cc9d67e8981ecdfa3aada
nisha783 <hm6980097@gmail.com>
2025-02-18 15:10:37 -0700

    hero section main

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,7 @@
 
  @yield('content')
 
-    <footer class="bg-dark text-white text-center py-3">
-        &copy; 2025 MySite. All rights reserved.
-    </footer>
+  
 
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,68 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="main-section">
-    <div class="container">
-        <div class="content-wrapper text-center py-5">
-            <h1 class="display-4 mb-4">CONNECT PRIVATELY WITH CRUSH</h1>
-            <h2 class="h3 mb-5">INFLUENCERS AND MODELS</h2>
-            <button class="btn btn-primary btn-lg">Sign Up</button>
-        </div>
+    <section class="hero-section"
+        style="background: url('{{ Vite::asset("resources/images/bg.png") }}') no-repeat center center/cover;">
+        <div class="container-fluid mt-5">
+            <div class="d-flex  justify-content-between">
+                <div class="d">
+                    <a href="" class=""  style="margin-left: 70px;">
+                        <img src="{{ Vite::asset("resources/images/send.png") }}" alt="">
+                    </a>
+                </div>
+                <div class="mt-3">
+                    <a href="" class="ms-4">
+                        <img src="{{ Vite::asset("resources/images/feed.png") }}" alt="">
+                    </a>
+                    <a href="" class="ms-5">
+                        <img src="{{ Vite::asset("resources/images/create.png") }}" alt="">
+                    </a>
+                    <a href="" class="ms-5" style="margin-right: 70px;">
+                        <img src="{{ Vite::asset("resources/images/signin.png") }}" alt="">
+                    </a>
+                </div>
+            </div>
 
-        <div class="row justify-content-center mt-5">
-            <div class="col-md-4">
-                <div class="card">
-                    <img src="{{ asset('img/test/group4.png') }}" class="card-img-top" alt="Influencer 1">
-                    <div class="card-body">
-                        <h5 class="card-title">Emily</h5>
+            <!-- Hero Text -->
+                <div class="row vh-100 d-flex align-items-center justify-content-center text-center">
+                    <div class="col-md-8 text-white" style="margin-top: 200px;">
+                        <h1 class="hero-title">CONNECT PRIVATELY WITH CRUSH INFLUENCERS AND MODELS</h1>
+                        <a href="">
+                            <img src="{{ Vite::asset("resources/images/signin.png") }}" alt="">
+                        </a>
+                    </div>
+                    <div class="col-12">
+                        <div class="row position-relative ">
+                            <!-- Left Box -->
+                            <div class="col-2" style="margin-top: 200px;">
+                                <a href="">
+                                    <img src="{{ Vite::asset('resources/images/Rectangle 10.png') }}"
+                                        class="img-fluid " alt="">
+                                </a>
+                            </div>
+        
+                            <!-- Center Images -->
+                            <div class="col-8 position-relative text-center">
+                                <a href="">
+                                    <img src="{{ Vite::asset('resources/images/Group 21.png') }}" class="img-fluid w-100"
+                                        alt="">
+                                </a>
+                            </div>
+        
+                            <!-- Right Box -->
+                            <div class="col-2 d-flex align-items-start justify-content-end"  style="margin-top: 200px;">
+                                <a href="">
+                                    <img src="{{ Vite::asset('resources/images/Rectangle 9.png') }}" class="img-fluid w-100"
+                                        alt="">
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <img src="{{ asset('img/test/group4.png') }}" class="card-img-top" alt="Influencer 2">
-                    <div class="card-body">
-                        <h5 class="card-title">Emily</h5>
-                    </div>
-                </div>
-            </div>
+
+
+
         </div>
-    </div>
-</section>
+        </div>
+    </section>
+@endsection
